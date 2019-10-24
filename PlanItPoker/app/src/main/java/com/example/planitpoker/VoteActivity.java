8ebc157c0cfa7a8369790510667c97824e2a0606
tundibogyo <tundekoncsard3566@gmail.com>
2019-10-24 22:16:54 +0300

    A masodik activity-ben, es az elso fragment-ben, mukodik minden, ami nem DB. Amit oda kellene beszurni, azt kiirom a Log-ba

--- PlanItPoker/app/src/main/java/com/example/planitpoker/VoteActivity.java
+++ PlanItPoker/app/src/main/java/com/example/planitpoker/VoteActivity.java
@@ -1,24 +1,45 @@
 package com.example.planitpoker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+
+import java.nio.BufferUnderflowException;
 
 
 public class VoteActivity extends AppCompatActivity
 {
+    String loginName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
+        Intent intent = getIntent();
+        loginName = intent.getStringExtra("Login_name");
+        Log.i("Sent through intent: ",loginName);//mukodik
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        //does not work!!!
         //ft.replace(R.id.fg_placeholder,new VoteFragment());
         //ft.commit();
+
         VoteFragment voteFragment = new VoteFragment();
+        //send loginName to the fragment
+        Bundle data = new Bundle();
+        data.putString("name",loginName);
+        voteFragment.setArguments(data);
         ft.add(R.id.fg_placeholder,voteFragment);
         ft.commit();
 
+    }
+    public String getLoginName()
+    {
+        return loginName;
     }
 }
--- PlanItPoker/app/src/main/java/com/example/planitpoker/VoteFragment.java
+++ PlanItPoker/app/src/main/java/com/example/planitpoker/VoteFragment.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,13 +18,23 @@ import java.util.Arrays;
 
 public class VoteFragment extends Fragment
 {
-
+    private int pressed_button_id;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
+        //inflate the layout for this fragment (don't forget to set the last param to false !!!)
         View retView = inflater.inflate(R.layout.vote_fragment,container,false);
 
-        ArrayList<String> buttonText = new ArrayList<>(Arrays.asList("0","1/2","1","2","3","5","8","13","20","40","100","?"));
+        //get login name form VoteActivity
+        //Bundle bundle = this.getArguments();
+        //final String loginName = bundle.getString("data");
+        //Log.i("Sent through args"," "+getArguments().getString("date")); //nem mukodik
+        VoteActivity voteActivity= (VoteActivity) getActivity();
+        final String loginName= voteActivity.getLoginName();
+
+        //text of buttons in arraylist
+        final ArrayList<String> buttonText = new ArrayList<>(Arrays.asList("0","1/2","1","2","3","5","8","13","20","40","100","?"));
+
         GridLayout myGridLayout =retView.findViewById(R.id.ly_grid);
 
         //fill grid with buttons
@@ -32,32 +43,65 @@ public class VoteFragment extends Fragment
             Button myButton = new Button(getContext());
 
             myButton.setId(i);
-            String btn_text = buttonText.get(i).toString();
+            String btn_text = buttonText.get(i);
             myButton.setText(btn_text);
             myGridLayout.addView(myButton);
         }
+        //instead of creating a space view, set the position of view in the gridlayout
         //Space space = new Space(getContext());
         //myGridLayout.addView(space);
-        ImageButton myButton = new ImageButton(getContext());
-        myButton.setLayoutParams(new GridLayout.LayoutParams());
+
+        //the last button contains an image, has to be managed differently form other buttons
+        ImageButton btn_coffe = new ImageButton(getContext());
+        btn_coffe.setLayoutParams(new GridLayout.LayoutParams());
         int btn_id=12;
+        //it will be placed in the 4th row, and 1st column (5,2)
         GridLayout.LayoutParams params = new GridLayout.LayoutParams(GridLayout.spec(4),GridLayout.spec(1));
-        GridLayout.LayoutParams param = new GridLayout.LayoutParams();
-        myButton.setLayoutParams(params);
-        myButton.setId(btn_id);
+        btn_coffe.setLayoutParams(params);
+        btn_coffe.setId(btn_id);
         Drawable dr = getResources().getDrawable(R.drawable.coffe,null);
         Bitmap bitmap = ((BitmapDrawable)dr).getBitmap();
         Drawable d = new BitmapDrawable(getResources(),Bitmap.createScaledBitmap(bitmap,50,50,true));
-        myButton.setImageDrawable(d);
-        myGridLayout.addView(myButton);
+        btn_coffe.setImageDrawable(d);
+        myGridLayout.addView(btn_coffe);
 
-
-        for(int i = 0; i < 13; ++i)
+        //whichever button was pressed last, its id will be stored in pressed_button_id
+        btn_coffe.setOnClickListener(new View.OnClickListener()
         {
-            myButton = retView.findViewById(i);
-            myButton.setOnClickListener();
+            @Override
+            public void onClick(View v)
+            {
+                pressed_button_id = 12;
+            }
+        });
+
+
+        for(int i = 0; i < 12; ++i)
+        {
+            final Button button = retView.findViewById(i);
+            button.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    v.setSelected(true);
+                    pressed_button_id=button.getId();
+                }
+            });
         }
 
+        Button btn_vote = new Button(getContext());
+        btn_vote = retView.findViewById(R.id.btn_Vote);
+        btn_vote.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view)
+            {
+                String msg = loginName + " " + buttonText.get(pressed_button_id);
+                Log.i("Adatbazisba: ",msg);
+            }
+        });
         return retView;
     }
 
